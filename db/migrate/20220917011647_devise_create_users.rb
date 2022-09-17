85830de596bb50acc3126ddf67588653c5ca72d7
tkhstty <tkhstty@gmail.com>
2022-09-17 11:12:59 +0900

    usersテーブルの姓名カラム名変更
    family_nameとlast_nameで両方名字になっていたため、first_nameとlast_nameに変更

--- db/migrate/20220917011647_devise_create_users.rb
+++ db/migrate/20220917011647_devise_create_users.rb
@@ -7,10 +7,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
       t.string :nickname, null: false
-      t.string :family_name, null: false
       t.string :last_name, null: false
-      t.string :family_name_reading, null: false
+      t.string :first_name, null: false
       t.string :last_name_reading, null: false
+      t.string :first_name_reading, null: false
       t.date   :birth_date, null: false
 
       ## Recoverable
--- db/schema.rb
+++ db/schema.rb
@@ -21,10 +21,10 @@ ActiveRecord::Schema.define(version: 2022_09_17_011647) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "nickname", null: false
-    t.string "family_name", null: false
     t.string "last_name", null: false
-    t.string "family_name_reading", null: false
+    t.string "first_name", null: false
     t.string "last_name_reading", null: false
+    t.string "first_name_reading", null: false
     t.date "birth_date", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
