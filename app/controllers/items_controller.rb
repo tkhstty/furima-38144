087417204d3c2ef9546d0e1017bb723a4c56759c
tkhstty <tkhstty@gmail.com>
2022-09-19 13:38:16 +0900

    商品出品画面の作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,24 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :category_id, :condition_id, :delivery_charge_id, :prefecture_id,
+                                 :delivery_duration_id, :price, :image).merge(user_id: current_user.id)
+  end
 end
