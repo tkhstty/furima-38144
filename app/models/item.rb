bd7b0daceb54ad40d6b63343b76bae431f1f7782
tkhstty <tkhstty@gmail.com>
2022-09-20 12:47:05 +0900

    商品に紐付いた各モデルアソシエーション追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -15,4 +15,11 @@ class Item < ApplicationRecord
 
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_charge
+  belongs_to :prefecture
+  belongs_to :delivery_duration
 end
