2fc1c0b385d99d3bc5af2fed93b1fde9c65f11dd
tkhstty <tkhstty@gmail.com>
2022-09-20 16:00:21 +0900

    コードレビュー後の修正
    - items_controller
      - authenticate_user!にdestroyアクションを追加
      - destroyアクションに出品者でなければ、destoryに至らない条件分岐を追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :authenticate_user!, only: [:new, :edit, :destroy]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
@@ -37,7 +37,9 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item.destroy
+    if current_user.id == @item.user.id
+      @item.destroy
+    end
     redirect_to root_path
   end
 
