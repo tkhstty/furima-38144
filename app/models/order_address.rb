09bf94eeaef40a2d9fbe3d7cd3f9bc23d8608e55
tkhstty <tkhstty@gmail.com>
2022-09-21 15:00:40 +0900

    order_addressモデルのエラーメッセージ修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -7,7 +7,7 @@ class OrderAddress
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)" }
     validates :city
     validates :house_number
-    validates :phone_number, numericality: { only_integer: true, message: "is invalid. Input only number" }, length: { minimum: 10, maximum: 11, message: "is too short" }
+    validates :phone_number, numericality: { only_integer: true, message: "is invalid. Input only number" }, length: { minimum: 10, maximum: 11, message: "is invalid. Input 10 or 11 character"}
     validates :user_id
     validates :item_id
     validates :token
